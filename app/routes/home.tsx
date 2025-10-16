ab65343d5cf6d4cc146062823f8b6d7d68aea2b4
Philippe Serhal <philippe.serhal@gmail.com>
2025-10-16 16:51:40 -0400

    feat: add a middleware example
    Support added in https://github.com/netlify/remix-compute/pull/546

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,27 @@
+import { netlifyRouterContext } from "@netlify/vite-plugin-react-router";
+
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
+
+const logMiddleware: Route.MiddlewareFunction = async ({
+  request,
+  context,
+}) => {
+  const country =
+    context.get(netlifyRouterContext).geo?.country?.name || "unknown location";
+  console.log(
+    `Handling ${request.method} request to ${request.url} from ${country}`,
+  );
+};
+
+export const middleware: Route.MiddlewareFunction[] = [logMiddleware];
 
 export default function Home() {
   return <Welcome />;
