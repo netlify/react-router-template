9be249a2d1cd53a86f859c8aaa20bad57ffc197a
Philippe Serhal <philippe.serhal@netlify.com>
2026-03-17 08:28:31 -0400

    chore(deps): upgrade to Vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), netlifyReactRouter(), netlify()],
+  plugins: [reactRouter(), netlifyReactRouter(), netlify()],
 });
